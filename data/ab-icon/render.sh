f47486dfa00b7376556034a3424c040dcfd55c43
Solra Bizna <solra@bizna.name>
2024-07-09 12:14:19 -0600

    Add app icon for macOS

--- data/ab-icon/render.sh
+++ data/ab-icon/render.sh
@@ -1,11 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
+function render() {
+    file=$1
+    w=$2
+    h=$3
+    margin=$4
+    if [ "$margin" = "" ]; then
+        margin=2.2
+    fi
+    if [ $file -ot logo.pov -o $file -ot $0 ]; then
+        povray logo.pov +W$w +H$h +O$file -D +UA +A0.05 Declare=MARGIN=$margin File_Gamma=sRGB
+    fi
+}
+
 # Windows icon
 for f in 16 32 48 128 256; do
-    if [ win$f.png -ot logo.pov -o win$f.png -ot $0 ]; then
-        povray logo.pov +W$f +H$f +Owin$f.png -D +UA +A0.05 Declare=MARGIN=2.2 File_Gamma=sRGB
-    fi
+    render win$f.png $f $f
 done
-convert win16.png win32.png win48.png win128.png win256.png ../../Resources/Windows/alephbet.ico
+convert win{16,32,48,128,256}.png ../../Resources/Windows/alephbet.ico
+
+if which iconutil &>/dev/null; then
+    mkdir -p AlephBet.iconset
+    for f in 16 32 128 256 512; do
+        render AlephBet.iconset/icon_${f}x${f}.png $f $f
+        render AlephBet.iconset/icon_${f}x${f}@2x.png $((f*2)) $((f*2))
+    done
+    iconutil -c icns AlephBet.iconset
+    mv AlephBet.icns ../../PBProjects/AlephBet.icns
+fi
